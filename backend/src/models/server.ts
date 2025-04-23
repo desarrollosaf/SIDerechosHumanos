d421c0651f53dd6fbe2fe71a649d591efb098f08
MarSGlez <martin.sanchezg@hotmail.com>
2025-04-23 10:06:30 -0600

    config express

---
+++ backend/src/models/server.ts
@@ -0,0 +1,55 @@
+import express, {Application} from 'express'
+import cors from 'cors'
+
+class Server {
+
+    private app: Application
+    private port: string
+    
+
+    constructor(){
+        this.app = express()
+        this.port = process.env.PORT || '3001'
+        this.listen();
+        this.midlewares();
+        this.router();
+        this.DBconnetc();
+        
+    }
+
+    listen(){
+        this.app.listen(this.port, () => {
+            console.log("La aplicación se esta corriendo exitosamente en el puerto => "+ this.port)           
+        })
+    }
+
+    router(){
+        
+    }
+
+    
+
+    midlewares(){
+        //Parseo BOdy
+        this.app.use(express.json())
+        this.app.use(cors())
+    }
+
+    async DBconnetc(){
+        try {
+
+            //await User.sync(); 
+            //await Servidor.sync(); 
+            console.log("Conexion de DB exitoso");
+
+        } catch (error) {
+            console.log("Conexion de DB errorena => "+error);
+            
+        }
+    }
+
+    
+}
+
+
+export default Server

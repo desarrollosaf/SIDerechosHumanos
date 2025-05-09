4dccfe72207ae9179d57f4187d68a07440c9ff19
MarSGlez <martin.sanchezg@hotmail.com>
2025-05-09 13:04:31 -0600

    cambios documetnos ts

--- backend/src/controllers/documentos.ts
+++ backend/src/controllers/documentos.ts
@@ -5,8 +5,29 @@ import  Solicitudes   from '../models/solicitud'
 
 export const saveDocumentos = async (req: Request, res: Response) => {
     const { body } = req;
+    const archivo = req.file;
+
+    if (!archivo) {
+        res.status(400).json({ message: 'Archivo no recibido' });
+    }
+
     const solicitud: any = await Solicitudes.findOne({ where: { userId: 9 } })
-    console.log(solicitud)
+
+    if (!solicitud) {
+         res.status(404).json({ message: 'Solicitud no encontrada' });
+    }
+
+    const nuevoDocumento = await Documentos.create({
+        solicitudId: solicitud.id,
+        path: 'archivo.path',
+        tipoDocumento: "1",
+    });
+
+    res.status(201).json({
+        message: 'Documento guardado exitosamente',
+        documento: nuevoDocumento
+    });
 
 
 };
+

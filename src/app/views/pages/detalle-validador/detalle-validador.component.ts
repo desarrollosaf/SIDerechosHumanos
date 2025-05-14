57d1eb00b4ad27a059675947ddaedebd46376eaf
cescDG <cesar199403@hotmail.com>
2025-05-14 16:16:00 -0600

    validador1

---
+++ src/app/views/pages/detalle-validador/detalle-validador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-detalle-validador',
+  imports: [],
+  templateUrl: './detalle-validador.component.html',
+  styleUrl: './detalle-validador.component.scss'
+})
+export class DetalleValidadorComponent {
+
+}
---
+++ src/app/views/pages/lista-validador/lista-validador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-validador',
+  imports: [],
+  templateUrl: './lista-validador.component.html',
+  styleUrl: './lista-validador.component.scss'
+})
+export class ListaValidadorComponent {
+
+}

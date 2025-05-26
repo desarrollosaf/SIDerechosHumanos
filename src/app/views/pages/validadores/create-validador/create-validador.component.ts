9985cc34fca0f191d1c1ea841c52eb34dfbb28d8
cescDG <cesar199403@hotmail.com>
2025-05-26 12:23:48 -0600

    edit validador

--- src/app/views/pages/validadores/create-validador/create-validador.component.ts
+++ src/app/views/pages/validadores/create-validador/create-validador.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-import { RouterLink, Router } from '@angular/router';
+import { RouterLink, Router, ActivatedRoute } from '@angular/router';
 import {FormsModule, FormBuilder, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { UserService } from '../../../../service/user.service';
@@ -16,7 +16,10 @@ export class CreateValidadorComponent {
   formReg: FormGroup;
   public validadoresList: any[] = [];
   public _userService = inject(UserService);
-  constructor(private fb: FormBuilder,private router: Router){
+  id:any;
+  operacion: string = 'Registrar';
+ 
+  constructor(private fb: FormBuilder,private router: Router,  private  aRouter: ActivatedRoute){
     this.formReg = this.fb.group({
       ap_paterno:['', Validators.required],
       ap_materno:['', Validators.required],
@@ -27,8 +30,14 @@ export class CreateValidadorComponent {
         Validators.pattern(/^[A-Z]{1}[AEIOU]{1}[A-Z]{2}\d{2}(0[1-9]|1[0-2])(0[1-9]|[12]\d|3[01])[HM]{1}(AS|BC|BS|CC|CL|CM|CS|CH|DF|DG|GT|GR|HG|JC|MC|MN|MS|NT|NL|OC|PL|QT|QR|SP|SL|SR|TC|TS|TL|VZ|YN|ZS|NE)[B-DF-HJ-NP-TV-Z]{3}[0-9A-Z]{1}\d{1}$/)
       ]],
     });
+
+    this.id = aRouter.snapshot.paramMap.get('id');
   }
   ngOnInit(): void {
+    if(this.id != null){
+      this.operacion = 'Editar ';
+      console.log(this.operacion);
+    }
   }
   envio():void {      
     if (this.formReg.valid) {
